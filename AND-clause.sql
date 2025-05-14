5f550f75bd3d1078facbe85f7c32449cb9e35720
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-14 17:16:35 +0500

    learn limit ,order by asc , desc and apply some queries on table to get some records

--- AND-clause.sql
+++ AND-clause.sql
@@ -24,6 +24,13 @@ select * from students where marks between 400 and 500; -- record between this r
 select * from students where city in ("lahore","kasur");-- select record from only these valus
 select * from students where city not in ("lahore","kasur");-- select record from  other then thsese values
 select distinct city from students; -- only give unique values ignore duplicates
-select * from students limit 4; -- give just 4 records of data from table (give first four records from a table)
-select * from students where marks > 500 limit 3; -- only return three reocrds having marks > 500
+-- select * from students limit 4; -- give just 4 records of data from table (give first four records from a table)
+-- select * from students where marks > 500 limit 3; -- only return three reocrds having marks > 500
+select * from students order by marks asc; -- give records order by ascending order lower to higher
+select * from students order by city asc;-- city start with a come first then so on (a to z)
+select * from students  order by marks desc;-- give the records base on marks in descending order (higher to lower)
+select *  from students order by city desc;-- give the reocrds base on city in desceding order (z to a)
+-- select * from students order by marks desc limit 3;  --this query give the records of students with higher marks (top 3)
+
+
 
