06fe62d59d182d815c9132770e7a7dbd2bbc4579
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-14 13:00:19 +0500

    added employessData.sql file

---
+++ employessTable.sql
@@ -0,0 +1,25 @@
+create table employeesData (
+    id int,
+    name varchar(50),
+    gender varchar(6),
+    salary int,
+    city varchar(50),
+    address varchar(100),
+    primary key (id)-- we can also make primary key like this
+);
+insert into employeesData values(101,"Talha Nawaz","Male",90000,"Abbottabad","House #879 Toheed colony jhangi");
+insert into employeesData (id,name,gender,salary,city,address)
+    values
+    (107, "Talha Nawaz", "Male", 90000, "Abbottabad", "House #879 Toheed colony jhangi"),
+    (102, "Areeba Khan", "Female", 85000, "Lahore", "House #12 Model Town A Block"),
+    (103, "Usman Ali", "Male", 100000, "Karachi", "Flat #304 Gulshan-e-Iqbal Block 13D"),
+    (104, "Hira Sheikh", "Female", 75000, "Islamabad", "Street #45 G-10/2"),
+    (105, "Bilal Ahmed", "Male", 95000, "Peshawar", "House #50 Phase 3 Hayatabad");
+
+select * from employeesData;
+select * from employeesData where gender = "female" 
+select * from employeesData where gender = "male" -- case sensitive also work
+select * from employeesData where id = 101 
+select * from employeesData where name = "talha nawaz"
+select * from employeesData where salary > 80000
+select * from employeesData where salary < 80000
