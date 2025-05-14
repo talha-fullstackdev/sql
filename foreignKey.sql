997cd7a7a59e33f6e500e6b382ed0a2a5ac7ff67
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-14 14:16:18 +0500

    learn about default values in sql tables

--- foreignKey.sql
+++ foreignKey.sql
@@ -8,6 +8,7 @@ CREATE TABLE departments (
 CREATE TABLE employees (
     employee_id INT PRIMARY KEY,
     employee_name VARCHAR(100),
+    salary int DEFAULT 25000, -- here by default we provide a value if someone not inserted salary then by default it is 25000
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES departments(department_id) -- making foreign key here from departments table and department_id
 );
@@ -20,11 +21,12 @@ VALUES
 (3, 'Human Resources');
 
 -- Insert records into employees
-INSERT INTO employees (employee_id, employee_name, department_id)
+INSERT INTO employees (employee_id, employee_name, salary, department_id)
 VALUES
-(101, 'Alice Johnson', 1),
-(102, 'Bob Smith', 2),
-(103, 'Carol Williams', 1),
-(104, 'David Lee', 3);
+(101, 'Alice Johnson',default, 1), -- use default value of salary if salary is not inserted
+(102, 'Bob Smith',"70000", 2),
+(103, 'Carol Williams',default, 1),
+(104, 'David Lee',default, 3);
 select * from employees where department_id =1;
 select * from departments;
+select * from employees where employee_name= "david lee"
