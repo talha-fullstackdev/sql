c37806bb814aae2dd52d138d13d337f0fbe306ae
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-15 15:38:41 +0500

    learn diff between truncate and delete command

--- table-related-queries.sql
+++ table-related-queries.sql
@@ -12,11 +12,11 @@ INSERT INTO student (rollno, name, marks, grade, city) VALUES
 (104, 'dhruv', 96, 'A', 'Delhi'),
 (105, 'emanuel', 12, 'F', 'Delhi'),
 (106, 'farah', 82, 'B', 'Delhi');
-Alter (to change the schema of tha table)
-alter table student add column age int; -- add a column in table with name age and contain int type value
-alter table student add column age int not null default 20; -- add column ad also add a default value
-alter table student drop column grade; -- delete a column fromm student table
-alter table student add column age int,add column gender varchar(6),add column section char(1); -- add multiple column in a table
-alter table student drop column marks , drop column grade;-- delete multiples columns from table at once
-
-
+-- Alter (to change the schema of tha table)
+-- alter table student add column age int; -- add a column in table with name age and contain int type value
+-- alter table student add column age int not null default 20; -- add column ad also add a default value
+-- alter table student drop column grade; -- delete a column fromm student table
+-- alter table student add column age int,add column gender varchar(6),add column section char(1); -- add multiple column in a table
+-- alter table student drop column marks , drop column grade;-- delete multiples columns from table at once
+-- different between truncate and drop is that drop delete table and truncate delete data inside the table
+truncate table student; -- delete the data of the table
