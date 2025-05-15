8fded66f0948827332c0f5ca3ffcfbf50a65a4fc
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-15 14:12:19 +0500

    learn about deleting and updating Fk id in both tables

---
+++ cascading-for-FK.sql
@@ -0,0 +1,18 @@
+CREATE table department(
+    dep_id int primary key,
+    course_name varchar(50)
+);
+create table teachers (
+    teacher_id int primary key ,
+    teacher_name varchar(50),
+    dept_id int,
+    foreign key (dept_id) references department(dep_id)
+    on delete cascade --data form parrent table is deleted data from child table related with FK also deleted
+    on update cascade --data form parrent table is updated data from child table related with FK also updated
+);
+INSERT INTO department (dep_id,course_name) VALUEs(101,"Engineering"),(102,"Bussiness"),(103,"Arts");
+insert into teachers (teacher_id,teacher_name,dept_id) values (100,"waqaas",101),(200,"zubair",102),(300,"raza",103);
+-- delete from department where dep_id = 102;
+update department set dep_id = 1000 where dep_id = 101 ; -- updated the departmen id in both tables using FK
+-- SELECT * from department;
+select * from teachers;
--- foreign-key.sql
+++ foreign-key.sql
@@ -6,7 +6,6 @@ create table teachers (
     teacher_id int ,
     teacher_name varchar(50),
     dep_id int
-
 );
 INSERT INTO department (dep_id,course_name) VALUEs(101,"Engineering"),(102,"Bussiness"),(103,"Arts");
 insert into teachers (teacher_id,teacher_name) values (100,"waqaas"),(200,"zubair"),(300,"raza");
