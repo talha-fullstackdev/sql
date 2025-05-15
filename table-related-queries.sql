8dcccd9b95549c390b453fdcbb496afbb3cefb75
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-15 14:56:06 +0500

    updated

--- table-related-queries.sql
+++ table-related-queries.sql
@@ -14,6 +14,7 @@ INSERT INTO student (rollno, name, marks, grade, city) VALUES
 (106, 'farah', 82, 'B', 'Delhi');
 Alter (to change the schema of tha table)
 alter table student add column age int; -- add a column in table with name age and contain int type value
+alter table student add column age int not null default 20; -- add column ad also add a default value
 alter table student drop column grade; -- delete a column fromm student table
 alter table student add column age int,add column gender varchar(6),add column section char(1); -- add multiple column in a table
 alter table student drop column marks , drop column grade;-- delete multiples columns from table at once
