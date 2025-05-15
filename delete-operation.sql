2ef35dab4d4637a5d730f512181f1683eed6227a
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-15 13:00:32 +0500

    delete single and multiple records base on grade

--- delete-operation.sql
+++ delete-operation.sql
@@ -16,5 +16,7 @@ delete from student where rollno = 101; -- deleted the record of student with ro
 delete from student where rollno in (101,103,105);  -- delete record of multiple students
 delete from student where city = "delhi";
 delete from student where city in ("delhi","mumbai","hyderabad"); -- we can also provide a value which is not exists in table
-
+delete from student where marks <= 70; -- deleted the student from table whos marks less then 70
+delete from student where grade = "c"; -- delete record base on grade
+delete from student where grade in ("c","f"); -- delete multiple  record base on grade
 select * from student;
