3a70146c39ab8c2c5d41ba1b3253aa4739d011ba
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-15 16:53:33 +0500

    learn joins e.g inner join, left join, right join, full join

--- joins.sql
+++ joins.sql
@@ -12,3 +12,8 @@ insert into courses (student_id,course) values (101,"WEB"),(102,"python"),(103,"
 -- select * from courses;
 select * from student inner join courses on student.student_id = courses.student_id;
 select * from student as s inner join courses as c on s.student_id = c.student_id; -- here we can also give a default name to tables like this this is called alias in sql
+select * from student left join courses on student.student_id = courses.student_id; -- left joiin (note which table we want full data we write that table first like here from student left join courses)
+select * from  student right join courses on courses.student_id = student.student_id; --right join
+select * from student left join courses on student.student_id=courses.student_id
+    union -- full join we use unior in left jpoin and right join to make it full join
+select * from  student right join courses on courses.student_id=student.student_id; 
