291bcdecd83949ec360bd7adfb21d6565b81cb01
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-15 13:50:25 +0500

    learn some concepts of foreign key how to implement it

---
+++ foreign-key.sql
@@ -0,0 +1,14 @@
+CREATE table department(
+    dep_id int primary key,
+    course_name varchar(50)
+);
+create table teachers (
+    teacher_id int ,
+    teacher_name varchar(50),
+    dep_id int
+
+);
+INSERT INTO department (dep_id,course_name) VALUEs(101,"Engineering"),(102,"Bussiness"),(103,"Arts");
+insert into teachers (teacher_id,teacher_name) values (100,"waqaas"),(200,"zubair"),(300,"raza");
+select * from teachers;
+SELECT * from department;
