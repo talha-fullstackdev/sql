f3b1274c5a689cca9ef7695c3ac46bc407e95e8e
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-15 11:59:50 +0500

    start learning update method in sql

---
+++ update-operation.sql
@@ -0,0 +1,15 @@
+CREATE TABLE student (
+  rollno INT PRIMARY KEY,
+  name VARCHAR(50),
+  marks INT,
+  grade CHAR(1),
+  city VARCHAR(50)
+);
+INSERT INTO student (rollno, name, marks, grade, city) VALUES
+(101, 'anil', 78, 'C', 'Pune'),
+(102, 'bhumika', 93, 'A', 'Mumbai'),
+(103, 'chetan', 85, 'B', 'Mumbai'),
+(104, 'dhruv', 96, 'A', 'Delhi'),
+(105, 'emanuel', 12, 'F', 'Delhi'),
+(106, 'farah', 82, 'B', 'Delhi');
+update student set grade = "A+" where grade = "A"; -- update the grade from A to A+
