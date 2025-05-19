0b213acb065a2b7b531e6feb55347eabe77f5d82
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-19 12:25:40 +0500

    formated exclusive-joins.sql

--- exclusive-joins.sql
+++ exclusive-joins.sql
@@ -8,7 +8,7 @@ create table courses(
 );
 insert into student (student_id,student_name) values (101,"talha Nawaz"),(102,"daud"),(103,"shameel");
 insert into courses (student_id,course) values (101,"WEB"),(102,"python"),(103,"backend"),(104,"arts"),(105,"ACCA"),(106,"CA"),(107,"BBA");
-select * from student left join courses on student.student_id=courses.student_id where courses.student_id is not null; -- left excllusive joins (only get comman data between both remove null values)
-select * from student right join courses on student.student_id=courses.student_id where student.student_id is not null; -- right exclusive joins
+select * from student left join courses on student.student_id = courses.student_id where courses.student_id is not null; -- left excllusive joins (only get comman data between both remove null values)
+select * from student right join courses on student.student_id = courses.student_id where student.student_id is not null; -- right exclusive joins
 -- this joins will removes null values 
-select * from student right join courses on student.student_id=courses.student_id where student.student_id is  null;-- return those courses whihc do not have any student 
+select * from student right join courses on student.student_id = courses.student_id where student.student_id is  null;-- return those courses whihc do not have any student 
