759c967e221633e7a5bff002428b3a8ff57457fb
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-19 12:41:15 +0500

    practice-set-two.sql

--- practice-set-two.sql
+++ practice-set-two.sql
@@ -12,6 +12,6 @@ INSERT INTO student (rollno, name, marks, grade, city) VALUES
 (104, 'daud', 96, 'A', 'lahore'),
 (105, 'uzair', 12, 'F', 'kasur'),
 (106, 'shameel', 82, 'B', 'jhelum');
-delete from student where marks < 90; -- question one
-alter table student drop column grade; --question two
-alter table student change name full_name varchar(50);-- question three
+delete from student where marks < 90; -- question one (delete all students with less then 90 marks)
+alter table student drop column grade; --question two (delete grade column from student)
+alter table student change name full_name varchar(50);-- question three (change the name of column in student table)
