d13933aae3a30b1925f8e8e114eda15e66824db4
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-19 12:09:25 +0500

    revised delete-operation.sql

--- delete-operation.sql
+++ delete-operation.sql
@@ -14,10 +14,10 @@ INSERT INTO student (rollno, name, marks, grade, city) VALUES
 (106, 'farah', 82, 'B', 'Delhi');
 delete from student;-- this will delete all student data from table
 delete from student where rollno = 101; -- deleted the record of student with rollnum 101 
-delete from student where rollno in (101,103,105);  -- delete record of multiple students
-delete from student where city = "delhi";
+delete from student where rollno in (101, 103, 105);  -- delete record of multiple students
+delete from student where city = "delhi"; -- delete the students whose city is delhi
 delete from student where city in ("delhi","mumbai","hyderabad"); -- we can also provide a value which is not exists in table
-delete from student where marks <= 70; -- deleted the student from table whos marks less then 70
-delete from student where grade = "c"; -- delete record base on grade
+delete from student where marks <= 70; -- deleted the students from table whos marks less then 70
+delete from student where grade = "c"; -- delete record base on grade (delete all students who's grade is c)
 delete from student where grade in ("c","f"); -- delete multiple  record base on grade
 select * from student;
